a8bd3ccc2a6296c3ddc99457db8889a6f4ce6d58
Iryna Kolesnyk <80821846+IrynaKolesnyk@users.noreply.github.com>
2021-08-02 23:25:22 +0200

    Updates

--- static/js/main.c498dfa1.chunk.js
+++
@@ -1,2 +0,0 @@
-(this["webpackJsonpgoit-react-hw-06-phonebook"]=this["webpackJsonpgoit-react-hw-06-phonebook"]||[]).push([[0],{11:function(t,e,n){"use strict";n.r(e);var o=n(1),c=n.n(o),r=n(3),s=n.n(r),i=(n(8),n(9),n(0));var a=function(){return Object(i.jsx)("div",{className:"App"})};s.a.render(Object(i.jsx)(c.a.StrictMode,{children:Object(i.jsx)(a,{})}),document.getElementById("root"))},8:function(t,e,n){}},[[11,1,2]]]);
-//# sourceMappingURL=main.c498dfa1.chunk.js.map
